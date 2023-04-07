45b612b7d73ee52962e087a191a0b901a0908aec
niyasrad <niyasrad@gmail.com>
2023-04-07 17:23:19 +0530

    [Niyas] Added Sidebar

--- frontend/src/components/navbar/Navbar.tsx
+++ frontend/src/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ export default function Navbar() {
     const [sidebarOpen, setSidebarOpen] = useState(false)
 
     const handleOpen = () => setSidebarOpen(true)
+    const handleClose = () => setSidebarOpen(false)
 
     return (
         <>
@@ -28,12 +29,22 @@ export default function Navbar() {
                 </div>
                 {
                     sidebarOpen &&
-                    (
-                        <div className="navbar__right">
-                            <a href="#" className="navbar__link navbar__link--pink">Try</a>
-                            <a href="#" className="navbar__link">Support</a>
-                            <a href="#" className="navbar__link">About</a>
-                        </div>
+                    (   
+                        <>
+                            <div className="mobile-nav__wrapper" onClick={handleClose}></div>
+                            <div className="mobile-nav__sidebar">
+                                <div className="mobile-nav__options">
+                                    <h1>Xylium</h1>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" onClick={handleClose} viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="mobile-nav__close">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                    </svg>
+
+                                </div>
+                                <a href="#" className="navbar__link navbar__link--pink">Try</a>
+                                <a href="#" className="navbar__link">Support</a>
+                                <a href="#" className="navbar__link">About</a>
+                            </div>
+                        </>
                     )
                 }
                 
