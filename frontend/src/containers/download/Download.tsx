2ecd6ade8ebd1347027b69021302536231bafbd8
Niyas Hameed <niyasrad@gmail.com>
2023-04-29 11:06:15 +0530

    [Niyas] Horizontal Scrolling Patch + No Games Filter (#1)

--- frontend/src/containers/download/Download.tsx
+++ frontend/src/containers/download/Download.tsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import Profilebar from "../../components/profilebar/Profilebar";
+import X from '../../assets/X.svg'
 import './Download.css'
 
 const Download = React.forwardRef((props: any, ref: any) => {
@@ -29,24 +30,35 @@ const Download = React.forwardRef((props: any, ref: any) => {
                     <span className="download__heading">Xylium</span>
                     <Profilebar username={user ? user: 'tester'}/>
                 </div>
-                <div className="download__information">
-                    <div className="download__games-list">
-                    {
-                        userData.games.slice(0,2).map((game: any) => (
-                            <div className="download__recent-game">
-                                <img className="download__recent-image" src={game.img_icon_url === "" ? "https://cdn.discordapp.com/attachments/946407954180108328/1098972496453173389/sport.png" : "https://cdn.akamai.steamstatic.com/steam/apps/" + game.appid + "/header.jpg"}  alt={game.name} />
-                                <span className="download__hours-put">{game.playtime_2weeks} Minutes</span>
-                            </div>
-                        ))
-                    }
+                {
+                    userData.games.length > 0 ? (
+                        <div className="download__information">
+                            <div className="download__games-list">
+                            {
+                                userData.games.slice(0,2).map((game: any) => (
+                                    <div className="download__recent-game">
+                                        <img className="download__recent-image" src={game.img_icon_url === "" ? "https://cdn.discordapp.com/attachments/946407954180108328/1098972496453173389/sport.png" : "https://cdn.akamai.steamstatic.com/steam/apps/" + game.appid + "/header.jpg"}  alt={game.name} />
+                                        <span className="download__hours-put">{game.playtime_2weeks} Minutes</span>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                        <div className="download__diagnosis">
+                            <span className="download__dia-intro">
+                                You played, in the last two weeks  for over,
+                            </span>
+                            <span className="download__dia-timings">{userData.total2weekplay} Minutes</span>
+                        </div>
                     </div>
-                    <div className="download__diagnosis">
-                        <span className="download__dia-intro">
-                            You played, in the last two weeks  for over,
-                        </span>
-                        <span className="download__dia-timings">{userData.total2weekplay} Minutes</span>
-                    </div>
-                </div>
+                    ) : (
+                        <div className="download__no-games">
+                            <img  className="download__no-games-logo" src={X} alt="Xylium Logo" />
+                            <span className="download__no-games-heading">Owww.. Nothing to show</span>
+                            <span className="download__no-games-desc">Play more to get something here!</span>
+                        </div>
+                    )
+                }
+                
             </div>
         </div>
     )
