810fb8aaf04d16a9608382911054ed758f0c42ec
Niyas Hameed <niyasrad@gmail.com>
2023-07-29 11:47:16 +0530

    [Niyas][Feature] Implemented Partial Feature #35 - Settings Endpoints (#58)

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,12 +2,14 @@ const userRoutes = require('./user.routes')
 const steamRoutes = require('./steam.routes')
 const detailRoutes = require('./details.routes')
 const statsRoutes = require('./stats.routes')
+const settingsRoutes = require('./settings.routes')
 
 const combineRoutes = (app) => {
     app.use('/api', userRoutes)
     app.use('/user', steamRoutes)
     app.use('/detail', detailRoutes)
     app.use('/stats', statsRoutes)
+    app.use('/settings', settingsRoutes)
 }
 
 module.exports = combineRoutes
---
+++ backend/src/routes/settings.routes.js
@@ -0,0 +1,100 @@
+const router = require('express').Router()
+const bcrypt = require('bcrypt')
+
+const { authMiddleWare } = require('../utils/auth')
+const User = require('../models/user')
+const Detail = require('../models/detail')
+
+router.post('/change', authMiddleWare, async (req, res) => {
+
+    try {
+
+        const userDocument = await User.findOne({ username: req.username })
+
+        if (!userDocument) {
+            return res.status(400).json({
+                message: "User Not Found!"
+            })
+        }
+
+        if (req.body.username) {
+            return res.status(400).json({
+                message: "You cannot change your username!"
+            })
+        }
+
+        if (req.body.email) {
+            userDocument.email = req.body.email
+        }
+        if (req.body.password) {
+            const passHash = await bcrypt.hash(req.body.password, 10)
+            userDocument.password = passHash
+        }
+
+        await userDocument.save()
+
+        return res.status(200).json({
+            message: "Credentials Saved Successfully!"
+        })
+
+    } catch(err) {
+
+        console.log(err)
+
+        return res.status(500).json({
+            message: "Something went wrong!"
+        })
+
+    }
+    
+})
+
+router.post('/delete', authMiddleWare, async (req, res) => {
+
+    const user = await User.findOne({ username: req.username })
+
+    if (!user) {
+        return res.status(400).json({
+            message: "User not found!"
+        })
+    }
+
+    try {
+
+        User.deleteOne({ username: req.username })
+        .then(() => {
+            Detail.deleteOne({ username: req.username })
+            .then(() => {
+                return res.status(200).json({
+                    message: "Account Deleted Successfully!"
+                })
+            })
+            .catch((err) => {
+                console.log(err)
+                return res.status(400).json({
+                    message: "Could not delete details!"
+                })
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+            return res.status(400).json({
+                message: "Could not delete user!"
+            })
+        })
+
+    } catch(err) {
+
+        console.log(err)
+
+        return res.status(500).json({
+            message: "Something went wrong!"
+        })
+
+    }
+    
+
+})
+
+
+module.exports = router
